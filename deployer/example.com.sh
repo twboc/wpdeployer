7b7ace10f1fc77cab62783da9fbe34f5f0e9bb51
hubert <=>
2018-07-27 18:35:21 +0200

    fixed port bug

--- deployer/example.com.sh
+++ deployer/example.com.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export WP_name="woocommerce1"
-export WP_volume="$rootDir/volumes/$domain/woocommerce"
-export WP_volumePath="$DB_volume:/var/www/html"
-export WP_PORT='81:80'
+# export WP_name="woocommerce1"
+# export WP_volume="$rootDir/volumes/$domain/woocommerce"
+# export WP_volumePath="$DB_volume:/var/www/html"
+# export WP_PORT='81:80'
 
-export DB_name="woo_mariadb1"
-export DB_volume="$rootDir/volumes/$domain/mariadb"
-export DB_volumePath="$WP_volume:/var/lib/mysql"
-export DB_port='3301:3306'
+# export DB_name="woo_mariadb1"
+# export DB_volume="$rootDir/volumes/$domain/mariadb"
+# export DB_volumePath="$WP_volume:/var/lib/mysql"
+# export DB_port='3301:3306'
 
