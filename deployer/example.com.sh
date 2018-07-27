8ef0b4bd06a143dd2cd13e4e58cfbc874fce4efa
hubert <=>
2018-07-27 19:16:48 +0200

    example file fix

--- deployer/example.com.sh
+++ deployer/example.com.sh
@@ -2,11 +2,11 @@
 
 export WP_name="woocommerce1"
 export WP_volume="$rootDir/volumes/$domain/woocommerce"
-export WP_volumePath="$DB_volume:/var/www/html"
+export WP_volumePath="$WP_volume:/var/www/html"
 export WP_port='81:80'
 
 export DB_name="woo_mariadb1"
 export DB_volume="$rootDir/volumes/$domain/mariadb"
-export DB_volumePath="$WP_volume:/var/lib/mysql"
+export DB_volumePath="$DB_volume:/var/lib/mysql"
 export DB_port='3301:3306'
 
