75974d09bebfdef054bc9e6de85dbca6a6bda396
hubert <=>
2018-07-28 11:51:02 +0200

    comment out echos and exports

--- run.sh
+++ run.sh
@@ -25,25 +25,25 @@ do
     mkdir -p "$rootDir/domains/$domain"
     rm -rf $rootDir/domains/$domain/docker-compose.yml;
 
-    echo $WP_name
-    echo $WP_volume
-    echo $WP_volumePath
-    echo $WP_port
+    # echo $WP_name
+    # echo $WP_volume
+    # echo $WP_volumePath
+    # echo $WP_port
 
-    echo $DB_name
-    echo $DB_volume
-    echo $DB_volumePath
-    echo $DB_port
+    # echo $DB_name
+    # echo $DB_volume
+    # echo $DB_volumePath
+    # echo $DB_port
 
-    export WP_name
-    export WP_volume
-    export WP_volumePath
-    export WP_port
+    # export WP_name
+    # export WP_volume
+    # export WP_volumePath
+    # export WP_port
 
-    export DB_name
-    export DB_volume
-    export DB_volumePath
-    export DB_port
+    # export DB_name
+    # export DB_volume
+    # export DB_volumePath
+    # export DB_port
 
     envsubst < "$rootDir/deployer/template.yml" > "$rootDir/domains/$domain/docker-compose.yml";
 
