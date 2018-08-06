cff2ffc218165cd7f3a2f9d06f5306ff1efceff6
hubert <hubert.sobkow@gmail.com>
2018-08-06 17:26:13 +0200

    container names per domain

--- deployer/example.com.sh
+++ deployer/example.com.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export WP_name="woocommerce1"
+export WP_name="woocommerce"
 export WP_volume="$rootDir/volumes/$domain/woocommerce"
 export WP_volumePath="$WP_volume:/var/www/html"
 export WP_portOut='80'
 export WP_portIn='80'
 
-export DB_name="woo_mariadb1"
+export DB_name="mariadb"
 export DB_volume="$rootDir/volumes/$domain/mariadb"
 export DB_volumePath="$DB_volume:/var/lib/mysql"
 export DB_portOut='3301'
