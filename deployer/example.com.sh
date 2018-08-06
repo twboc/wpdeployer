255cbf0bdf6be6d7ce16b088557403b8ea28d952
hubert <hubert.sobkow@gmail.com>
2018-08-06 14:01:13 +0200

    pre clean up

--- deployer/example.com.sh
+++ deployer/example.com.sh
@@ -3,10 +3,12 @@
 export WP_name="woocommerce1"
 export WP_volume="$rootDir/volumes/$domain/woocommerce"
 export WP_volumePath="$WP_volume:/var/www/html"
-export WP_port='81:80'
+export WP_portOut='80'
+export WP_portIn='80'
 
 export DB_name="woo_mariadb1"
 export DB_volume="$rootDir/volumes/$domain/mariadb"
 export DB_volumePath="$DB_volume:/var/lib/mysql"
-export DB_port='3301:3306'
+export DB_portOut='3301'
+export DB_portIn='3306'
 
--- run.sh
+++ run.sh
@@ -3,7 +3,12 @@ rootDir=$(pwd);
 
 . $rootDir/deployer/scripts/utils.sh
 
+echo "https://github.com/docker/compose/releases/download/3.7.0/docker-compose-$(uname -s)-$(uname -m)"
+
 preventSubshell
+
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
 
 installPackageIfNotExists "curl"
 installPackageIfNotExists "docker"
@@ -14,10 +19,15 @@ mkdir -p $rootDir/volumes
 # start up the nginx container
 rm -rf $rootDir/deployer/nginx/docker-compose.yml;
 
-mkdir -p $rootDir/deployer/nginx/volume/
-mkdir -p $rootDir/deployer/nginx/volume/sites-available
-mkdir -p $rootDir/deployer/nginx/volume/sites-enabled
+# mkdir -p $rootDir/deployer/nginx/volume/
+# mkdir -p $rootDir/deployer/nginx/volume/sites-available
+# mkdir -p $rootDir/deployer/nginx/volume/sites-enabled
 
+
+sudo docker network create nginx-proxy
+
+envsubst < "$rootDir/deployer/nginx/template.yml" > "$rootDir/deployer/nginx/docker-compose.yml";
+sudo docker-compose -f "$rootDir/deployer/nginx/docker-compose.yml" up - d
 
 
 for file in $rootDir/configs/*
@@ -37,31 +47,24 @@ do
 
     envsubst < "$rootDir/deployer/template.yml" > "$rootDir/domains/$domain/docker-compose.yml";
 
-    sudo docker-compose -f "$rootDir/domains/$domain/docker-compose.yml" up -d
+    sudo docker-compose -f "$rootDir/domains/$domain/docker-compose.yml" up 
 
+    # envsubst '$domain $WP_portOut' < "$rootDir/deployer/nginx/serverTemplate" > "$rootDir/deployer/nginx/volume/sites-available/$domain";
 
-    
-
-    envsubst '$domain $WP_portOut' < "$rootDir/deployer/nginx/serverTemplate" > "$rootDir/deployer/nginx/volume/sites-available/$domain";
-
-    ln -s $rootDir/deployer/nginx/volume/sites-available/$domain $rootDir/deployer/nginx/volume/sites-enabled/$domain
-
-
+    # ln -s $rootDir/deployer/nginx/volume/sites-available/$domain $rootDir/deployer/nginx/volume/sites-enabled/$domain
 
     fi
 done
 
-NX_volume="$rootDir/deployer/nginx/volume/"
-NX_volumePath=$NX_volume":/etc/nginx/:ro"
-NX_volumeSA=$rootDir/deployer/nginx/volume/sites-available":/etc/nginx/sites-available:ro"
-NX_volumeSE=$rootDir/deployer/nginx/volume/sites-enabled":/etc/nginx/sites-enabled:ro"
+# NX_volume="$rootDir/deployer/nginx/volume/"
+# NX_volumePath=$NX_volume":/etc/nginx/:ro"
+# NX_volumeSA=$rootDir/deployer/nginx/volume/sites-available":/etc/nginx/sites-available:ro"
+# NX_volumeSE=$rootDir/deployer/nginx/volume/sites-enabled":/etc/nginx/sites-enabled:ro"
 
-export NX_volume;
-export NX_volumePath;
-export NX_volumeSA;
-export NX_volumeSE;
+# export NX_volume;
+# export NX_volumePath;
+# export NX_volumeSA;
+# export NX_volumeSE;
 
-cp $rootDir/deployer/nginx/nginx.conf $rootDir/deployer/nginx/volume/nginx.conf
+# cp $rootDir/deployer/nginx/nginx.conf $rootDir/deployer/nginx/volume/nginx.conf
 
-envsubst < "$rootDir/deployer/nginx/template.yml" > "$rootDir/deployer/nginx/docker-compose.yml";
-sudo docker-compose -f "$rootDir/deployer/nginx/docker-compose.yml" up -d
