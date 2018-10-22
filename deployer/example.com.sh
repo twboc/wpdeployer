2cb79cd3307f0ccfc8fe24609fced57d7bb228df
hubert <hubert.sobkow@gmail.com>
2018-10-22 19:11:52 +0200

    fixed example files

--- deployer/example.com.sh
+++ deployer/example.com.sh
@@ -5,13 +5,13 @@ export HOST_onlySubdomains=true             # true|false
 export HOST_subdomains=()                   #('subdomain1' 'subdomain2')
 
 export WP_name="woocommerce"
-export WP_volume="$rootDir/volumes/$domain/woocommerce"
+export WP_volume="$rootDir/volumes/$domainFile/woocommerce"
 export WP_volumePath="$WP_volume:/var/www/html"
 export WP_portOut='81'
 export WP_portIn='80'
 
 export DB_name="mariadb"
-export DB_volume="$rootDir/volumes/$domain/mariadb"
+export DB_volume="$rootDir/volumes/$domainFile/mariadb"
 export DB_volumePath="$DB_volume:/var/lib/mysql"
 export DB_portOut='3301'
 export DB_portIn='3306'
