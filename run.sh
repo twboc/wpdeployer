a4307fd87eadbe886b1971c329a1aa76e04ed247
hubert <hubert.sobkow@gmail.com>
2018-10-22 18:23:23 +0200

    Config files with subdomains

--- run.sh
+++ run.sh
@@ -3,6 +3,59 @@ rootDir=$(pwd);
 
 . $rootDir/deployer/scripts/utils.sh
 
+resolveSubdomains(){
+    return_=0 
+
+    if [ -z "$HOST_www" ]; then 
+        echo "Variable $HOST_www is not set" 1>&2
+        echo "Setting $HOST_www to: true" 1>&2
+        HOST_www=true
+        echo "Including the www subdomain" 1>&2
+        HOST_subdomains+=('www')
+    else 
+        echo "WWW subdomain is included" 1>&2
+        if [ "$HOST_www" = true ] ; then
+            echo "Including the www subdomain" 1>&2
+            HOST_subdomains+=('www')
+        fi
+    fi
+
+    if [ "$HOST_onlySubdomains" = true ]; then
+        echo "Omitting SLD without subdomain" 1>&2
+    else
+        echo "Adding SLD without subdomain" 1>&2
+        HOST_domains+=($domain)
+    fi
+
+    for subdomain in "${HOST_subdomains[@]}"
+    do
+        echo "Adding subdomain $subdomain.$domain to HOST_domains" 1>&2
+        HOST_domains+=("$subdomain.$domain")
+    done
+
+    if [ ${#HOST_domains[@]} -eq 0 ]; then
+        echo "" 1>&2
+        echo "!!!!! ERROR !!!!!" 1>&2
+        echo "" 1>&2
+        echo "Configuration did not create a list of domains" 1>&2
+        echo "Domain $domain configuration file declares no domains for HOST_domainsDeclaration variable" 1>&2
+        echo "Please check your config if HOST_www, HOST_onlySubdomains, HOST_subdomains variables" 1>&2
+        echo "HOST_onlySubdomains set to true and empty HOST_subdomains creates no variables" 1>&2
+        echo "" 1>&2
+        local return_=1;
+    else
+        echo "Cofiguration created a list of domains" 1>&2
+        HOST_domainsDeclaration=$(printf ", %s" "${HOST_domains[@]}")
+        HOST_domainsDeclaration=${HOST_domainsDeclaration:1}
+        echo "HOST_domainsDeclaration: " 1>&2
+        echo $HOST_domainsDeclaration 1>&2
+        local return_=0;
+    fi
+
+    echo "$return_"
+
+}
+
 preventSubshell
 
 sudo docker stop $(sudo docker ps -a -q)
@@ -42,6 +95,9 @@ do
 
     domain=$(basename $file .sh)
     export domain
+    export HOST_domains=()
+    export HOST_subdomains=()
+    export HOST_domainsDeclaration=""
 
     . $rootDir/configs/$domain.sh --source-only
 
@@ -51,9 +107,14 @@ do
     mkdir -p "$rootDir/domains/$domain"
     rm -rf $rootDir/domains/$domain/docker-compose.yml;
 
-    envsubst < "$rootDir/deployer/template.yml" > "$rootDir/domains/$domain/docker-compose.yml";
-
-    sudo docker-compose -f "$rootDir/domains/$domain/docker-compose.yml" up -d
-
+    if [ $(resolveSubdomains) -eq 0 ]; then
+        envsubst < "$rootDir/deployer/template.yml" > "$rootDir/domains/$domain/docker-compose.yml";
+        sudo docker-compose -f "$rootDir/domains/$domain/docker-compose.yml" up -d
+    else
+        echo ""
+        echo "Domains and subdomains not created for $domain"
+        echo "Omitting container configuration for $(basename $file)"
+    fi
+    
     fi
 done
