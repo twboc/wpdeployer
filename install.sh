9e4ffe32e2d6faf4c3a3373429caebd4878fa773
twobc <hubert.sobkow@gmail.com>
2020-10-28 16:42:34 +0100

    INstalll script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get -y update
 
-sudo apt-get install \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -11,18 +11,18 @@ sudo apt-get install \
     
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     
-sudo apt-key fingerprint 0EBFCD88
+sudo apt-key -y fingerprint 0EBFCD88
        
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
    
-sudo apt-get update
+sudo apt-get -y update
    
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
    
-sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
+sudo apt-get -y install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
    
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   
